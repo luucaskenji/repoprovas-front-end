9ac73da07ef62bef2831e03e5d38e437fd9ddc66
Lucas Murakami <lkenjim98@gmail.com>
2020-12-17 17:21:24 -0400

    getting courses from server and displaying options

--- src/pages/NewExamForm/index.js
+++ src/pages/NewExamForm/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import {
+    Container,
+    SelectContainer,
+    InputContainer,
+    Button
+} from './FormStyles';
 
 export default function NewExamForm() {
+    const [courses, setCourses] = useState([]);
+    console.log(courses);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/courses')
+            .then(r => setCourses(r.data))
+            .catch(err => console.log(err));
+    }, []);
+
     return (
         <Container>
             <form>
@@ -10,7 +26,9 @@ export default function NewExamForm() {
                         <SelectContainer>
                             <label>Matéria</label>
                             <select>
-                                <option>Teste</option>
+                                {
+                                    courses.map(c => <option>{c.name}</option>)
+                                }
                             </select>
                         </SelectContainer>
     
@@ -44,71 +62,3 @@ export default function NewExamForm() {
         </Container>
     );
 }
-
-const Container = styled.div`
-    width: 100%;
-    height: calc(100vh - 80px);
-    display: flex;
-    align-items: center;
-    font-family: 'Patrick Hand', cursive;
-    color: #EFEFEF;
-    padding: 0 40px;
-    
-    form {
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        
-        & > div:first-child {
-            width: 65%;
-
-            & > div {
-                display: flex;
-                justify-content: space-around;
-            }
-        }
-
-        & > div:last-child {
-            flex-grow: 1;
-            text-align: center;
-        }
-    }
-`;
-
-const SelectContainer = styled.div`
-    display: inline-block;
-    margin-bottom: 45px;
-    font-size: 1.17rem;
-
-    select {
-        display: block;
-        width: 200px;
-        height: 45px;
-        margin-top: 15px;
-        outline: none;
-    }
-`;
-
-const InputContainer = styled.div`
-    display: inline-block;
-    font-size: 1.17rem;
-
-    input {
-        display: block;
-        width: 200px;
-        height: 45px;
-        margin-top: 15px;
-        outline: none;
-    }
-`;
-
-const Button = styled.button`
-    width: 146px;
-    height: 55px;
-    border-radius: 10px;
-    background: green;
-    color: white;
-    font-family: 'Patrick Hand', cursive;
-    font-size: 1.17rem;
-`;
